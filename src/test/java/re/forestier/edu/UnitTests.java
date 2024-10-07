eb133308fc9ecf60a80d83aef6568c317658aba4
mohaTlg <mamadoumoussa.diallo.9212@gmail.com>
2024-10-07 08:21:25 +0200

    test player done

--- src/test/java/re/forestier/edu/UnitTests.java
+++ src/test/java/re/forestier/edu/UnitTests.java
@@ -1,6 +1,8 @@
 package re.forestier.edu;
 
 import org.junit.jupiter.api.*;
+
+import re.forestier.edu.rpg.UpdatePlayer;
 import re.forestier.edu.rpg.player;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
@@ -39,10 +41,28 @@ public class UnitTests {
             p.addMoney(45);
             p.getXp();
             p.removeMoney(45);
+            
+            UpdatePlayer.addXp(p, 30);
+            p.retrieveLevel();            
+
+            UpdatePlayer.addXp(p, 50); //level 4
+            p.retrieveLevel();            
+            
+            UpdatePlayer.addXp(p, 120); //level 5
+            p.retrieveLevel();            
         } catch (IllegalArgumentException e) {
             return;
         }
         assertThat(p.money, is(100));
+    }
+
+    @Test
+    @DisplayName("avatar class test")
+    void avatarClass() {
+        player player = new player("Florian", "Grognak le barbare", "ADvendu", 100, new ArrayList<>());
+        player p1 = new player("Florian", "Grognak le barbare", "ARCHER", 100, new ArrayList<>());
+    
+        
     }
 
 
